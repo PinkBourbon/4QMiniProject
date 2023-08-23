fd03a1365353564b1128e07157bf66f4dce523d8
Apto <cgyk.piro@gmail.com>
2023-08-23 20:51:50 +0900

    gameScene에 Plant 게임 오브젝트 하나 추가.

--- Acquitaine Engine/Acquitaine Client/GameScene.cpp
+++ Acquitaine Engine/Acquitaine Client/GameScene.cpp
@@ -1,11 +1,13 @@
 #include "GameScene.h"
 
 #include "SpaceShip.h"
+#include "Planet.h"
 
 GameScene::GameScene()
 {
 	this->_SceneName = "GameScene";
 
 	AddObject<SpaceShip>("spaceShip");
+	AddObject<Planet>("plant1");
 }
 
